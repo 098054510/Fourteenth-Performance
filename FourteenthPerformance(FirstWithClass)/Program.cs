9b3f490fadae6833261eaea4d6306e1e444e8199
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-11-06 08:34:28 -0300

    Fourteenth Performance/Program made in C#

---
+++ FourteenthPerformance(FirstWithClass)/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace FourteenthPerformance_FirstWithClass_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker W;
+            double desconto, aumento;
+
+            W = new Worker();
+
+            Console.WriteLine("-----------------------------------------------------------------------");
+            Console.WriteLine("---------------------------SALAROMETRO 2018----------------------------");
+            Console.WriteLine("-----------------------------------------------------------------------");
+
+            Console.WriteLine("Siga os passos descritos e tudo ficará bem");
+            Console.WriteLine("Caso não queira aumentar ou descontar, insira '0.00' e tudo ficará bem.");
+            Console.WriteLine("Digite o Nome do Funcionario: ");
+            W.nome = Console.ReadLine();
+            Console.WriteLine("Digite o Salario do Funcionado: ");
+            W.salarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Descontar salario em quanto?: ");
+            desconto = double.Parse(Console.ReadLine());
+            W.SalarioLiquido(desconto);
+
+            Console.WriteLine("Dados do funcionario: " + W);
+            Console.WriteLine("Pressione ENTER Para continuar");
+            Console.ReadLine();
+
+            Console.WriteLine("Aumentar salario em quanto?: "); Console.WriteLine("Insira '0.00' caso nao queira aumentar o salario.");
+            aumento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            W.NovoSalario(aumento);
+
+            Console.WriteLine("Dados do funcionario: " + W);
+            Console.WriteLine("Pressione ENTER para finalizar");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FourteenthPerformance(FirstWithClass)/Worker.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace FourteenthPerformance_FirstWithClass_
+{
+    class Worker
+    {
+        public string nome;
+        public double salarioBruto;
+        public double desconto;
+        public double aumento;
+        public double salarioAtual;
+
+        public double Salario()
+        {
+            return salarioBruto;
+        }
+        public void SalarioLiquido(double desconto)
+        {
+            salarioAtual = salarioBruto - desconto;
+        }
+
+        public void NovoSalario(double aumento)
+        {
+            salarioAtual = salarioBruto + aumento;
+        }
+
+        public override string ToString()
+        {
+            return nome +
+                ", R$ " +
+                salarioAtual.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
